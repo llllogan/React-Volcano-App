52b1ddda25e3f99b59fb15a766ce71679af8433c
llllogan <logan.janssen@icloud.com>
2024-04-04 20:50:45 +1000

    Fixing the Volcano class

--- cab230client/src/packages/Volcano.ts
+++ cab230client/src/packages/Volcano.ts
@@ -6,91 +6,60 @@ class Volcano {
 
     @Expose({ name: 'id' })
     @IsNumber()
-    Id: number;
+    Id!: number;
 
     @Expose({ name: 'name' })
     @IsString()
-    Name: string;
+    Name!: string;
 
     @Expose({ name: 'country' })
     @IsString()
-    Country: string;
+    Country!: string;
 
     @Expose({ name: 'region' })
     @IsString()
-    Region: string;
+    Region!: string;
 
     @Expose({ name: 'subregion' })
     @IsString()
-    Subregion: string;
+    Subregion!: string;
 
     @Expose({ name: 'last_eruption' })
     @IsString()
-    LastEruption: string;
+    LastEruption!: string;
 
     @Expose({ name: 'summit' })
     @IsNumber()
-    Summit: number;
+    Summit!: number;
 
     @Expose({ name: 'elevation' })
     @IsNumber()
-    Elevation: number;
+    Elevation!: number;
 
     @Expose({ name: 'latitude' })
     @IsNumber()
-    Latitude: number;
+    Latitude!: number;
 
     @Expose({ name: 'longitude' })
     @IsNumber()
-    Longitude: number;
+    Longitude!: number;
 
     @Expose({ name: 'population_5km' })
     @IsNumber()
-    PopulationWithin5km: number;
+    PopulationWithin5km: number | undefined;
 
     @Expose({ name: 'population_10km' })
     @IsNumber()
-    PopulationWithin10km: number;
+    PopulationWithin10km: number | undefined;
 
     @Expose({ name: 'population_30km' })
     @IsNumber()
-    PopulationWithin30km: number;
+    PopulationWithin30km: number | undefined;
 
     @Expose({ name: 'population_100km' })
     @IsNumber()
-    PopulationWithin100km: number;
+    PopulationWithin100km: number | undefined;
 
-    constructor(
-        Id: number, 
-        Name: string, 
-        Country: string, 
-        Region: string, 
-        Subregion: string, 
-        LastEruption: string, 
-        Summit: number, 
-        Elevation: number, 
-        Latitude: number, 
-        Longitude: number, 
-        PopulationWithin5km: number, 
-        PopulationWithin10km: number, 
-        PopulationWithin30km: number, 
-        PopulationWithin100km: number
-    ) {
-        this.Id = Id;
-        this.Name = Name;
-        this.Country = Country;
-        this.Region = Region;
-        this.Subregion = Subregion;
-        this.LastEruption = LastEruption;
-        this.Summit = Summit;
-        this.Elevation = Elevation;
-        this.Latitude = Latitude;
-        this.Longitude = Longitude;
-        this.PopulationWithin5km = PopulationWithin5km;
-        this.PopulationWithin10km = PopulationWithin10km;
-        this.PopulationWithin30km = PopulationWithin30km;
-        this.PopulationWithin100km = PopulationWithin100km;
-    }
 }
 
 export default Volcano;
