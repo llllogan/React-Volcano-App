edd6ec32ed293b5087b3e928a7d029131b173431
llllogan <logan.janssen@icloud.com>
2024-04-08 20:13:01 +1000

    Added more dependencies

--- cab230client/src/components/rightContainer/VolcanoList.tsx
+++ cab230client/src/components/rightContainer/VolcanoList.tsx
@@ -1,15 +1,30 @@
 import { useContext } from "react";
 import { CountryContext, CountryContextType } from "../../packages/Context";
+import { AgGridReact } from "ag-grid-react";
 
 
 export default function VolcanoList() {
 
     const { selectedCountry } = useContext(CountryContext) as CountryContextType;
 
+    const rowData = [
+        {make: 'Toyota', model: 'Celica', price: 35000},
+        {make: 'Ford', model: 'Mondeo', price: 32000},
+        {make: 'Porsche', model: 'Boxster', price: 72000}
+    ];
+    const columnDefs = [
+        {field: 'make'},
+        {field: 'model'},
+        {field: 'price'}
+    ];
+
     if (selectedCountry) {
         return (
             <div>
                 <h1>{selectedCountry.name}</h1>
+                <AgGridReact 
+                    rowData={rowData}
+                    columnDefs={columnDefs}/>
             </div>
         );
     } else {
