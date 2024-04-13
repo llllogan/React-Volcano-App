44a92352d0dc02c8c5cc43f66b208cb8e20c7acf
Logan <logan@codify.com>
2024-04-14 09:20:54 +1000

    Added radius circles to the map with flyouts with text

--- cab230client/src/components/rightContainer/singleVolcano/MapTest.tsx
+++ cab230client/src/components/rightContainer/singleVolcano/MapTest.tsx
@@ -1,35 +1,46 @@
-import { Circle, MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { Circle, MapContainer, Marker, Popup, TileLayer, Tooltip } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import { LatLngTuple } from "leaflet";
 
 export default function MapTest() {
+  const center: LatLngTuple = [51.505, -0.09];
+  const fillBlueOptions = { fillColor: "blue" };
 
-  const center: LatLngTuple = [51.505, -0.09]
-  const fillBlueOptions = { fillColor: 'blue' }
-
-  
   return (
     <div>
       <MapContainer
         className="leaflet-container"
         center={[51.505, -0.09]}
-        zoom={13}
+        zoom={8}
         scrollWheelZoom={false}
-        style={{ width: '100%', height: '600px' }}
+        style={{ width: "100%", height: "600px" }}
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={center}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
-        <Circle center={center} pathOptions={fillBlueOptions} radius={5000} />
-        <Circle center={center} pathOptions={fillBlueOptions} radius={10000} />
-        <Circle center={center} pathOptions={fillBlueOptions} radius={30000} />
-        <Circle center={center} pathOptions={fillBlueOptions} radius={100000} />
+        <Circle center={center} pathOptions={fillBlueOptions} radius={100000}>
+        <Tooltip >
+            100km radius circle
+          </Tooltip>
+        </Circle>
+        <Circle center={center} pathOptions={fillBlueOptions} radius={30000}>
+        <Tooltip >
+            30km radius circle
+          </Tooltip>
+        </Circle>
+        <Circle center={center} pathOptions={fillBlueOptions} radius={10000}>
+        <Tooltip >
+            10km radius circle
+          </Tooltip>
+        </Circle>
+        <Circle center={center} pathOptions={fillBlueOptions} radius={5000}>
+          <Tooltip >
+            5km radius circle
+          </Tooltip>
+        </Circle>
+        
+        
       </MapContainer>
     </div>
   );
