863f0bc1ced0f335b796368ab9ffdffa91fc3a5d
Logan <logan@codify.com>
2024-04-16 08:28:16 +1000

    Added arrow svg to back to country button

--- cab230client/src/components/rightContainer/singleVolcano/SingleViewContainer.tsx
+++ cab230client/src/components/rightContainer/singleVolcano/SingleViewContainer.tsx
@@ -1,22 +1,33 @@
 import { useContext, useEffect, useState } from "react";
-import { CountryContext, CountryContextType, VolcanoClientContext, VolcanoClientContextType, VolcanoContext, VolcanoContextType, VolcanoSelectedContext, VolcanoSelectedContextType } from "../../../packages/Context";
+import {
+  CountryContext,
+  CountryContextType,
+  VolcanoClientContext,
+  VolcanoClientContextType,
+  VolcanoContext,
+  VolcanoContextType,
+  VolcanoSelectedContext,
+  VolcanoSelectedContextType,
+} from "../../../packages/Context";
 import Volcano from "../../../packages/Volcano";
 import Map from "./Map";
 import InformationContainer from "./InformationContainer";
 
 export default function SingleViewContainer() {
-  
   const { selectedCountry } = useContext(CountryContext) as CountryContextType;
   const { selectedVolcano } = useContext(VolcanoContext) as VolcanoContextType;
-  const { volcanoClient } = useContext(VolcanoClientContext) as VolcanoClientContextType;
+  const { volcanoClient } = useContext(
+    VolcanoClientContext
+  ) as VolcanoClientContextType;
 
-  const { setVolcanoSelected } = useContext(VolcanoSelectedContext) as VolcanoSelectedContextType;
-  
+  const { setVolcanoSelected } = useContext(
+    VolcanoSelectedContext
+  ) as VolcanoSelectedContextType;
+
   const [volcano, setVolcano] = useState<Volcano>(new Volcano(selectedVolcano));
   const [mapLoaded, setMapLoaded] = useState(false);
 
   useEffect(() => {
-    
     const getVolcanoFromApi = async () => {
       const volcanoData: Volcano = await volcanoClient.getVolcanoById(
         volcano.getId()
@@ -31,14 +42,31 @@ export default function SingleViewContainer() {
 
   return (
     <div>
-      {mapLoaded ? (<Map volcano={volcano}></Map>) : null}
+      {mapLoaded ? <Map volcano={volcano}></Map> : null}
       <h1 className="pt-4">{volcano.Name}</h1>
-      <InformationContainer volcano={volcano}/>
-      <button className="btn btn-outline-secondary" type="button" onClick={
-        () => {
+      <InformationContainer volcano={volcano} />
+      <button
+        className="btn btn-outline-secondary"
+        type="button"
+        onClick={() => {
           setVolcanoSelected(false);
-        }
-      }>Back to {selectedCountry.name}</button>
+        }}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="25"
+          height="25"
+          fill="currentColor"
+          className="bi bi-arrow-left"
+          viewBox="0 0 20 17"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8"
+          />
+        </svg>
+        Back to {selectedCountry.name}
+      </button>
     </div>
   );
 }
