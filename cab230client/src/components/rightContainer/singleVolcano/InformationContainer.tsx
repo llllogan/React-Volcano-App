933483481b02fa518215be7cabe5f3f5b76fcfcc
Logan <logan@codify.com>
2024-04-26 18:22:09 +1000

    Added bar chart to volcano view

--- cab230client/src/components/rightContainer/singleVolcano/InformationContainer.tsx
+++ cab230client/src/components/rightContainer/singleVolcano/InformationContainer.tsx
@@ -1,26 +1,61 @@
 import Volcano from "../../../packages/Volcano";
 import InformationBrick from "./InformationBrick";
+import { BarChart } from '@mui/x-charts/BarChart';
 
 interface Props {
-    volcano: Volcano;
+  volcano: Volcano;
 }
 
 export default function InformationContainer(props: Props) {
 
-    return (
-        <div className="row py-4">
-            <div className="col">
-                <InformationBrick title="country" information={props.volcano.Country} />
-                <InformationBrick title="region" information={props.volcano.Region} />
-                <InformationBrick title="subregion" information={props.volcano.Subregion} />
-            </div>
-            <div className="col">
-                <InformationBrick title="summit" information={props.volcano.getSummit() + "m"} />
-                <InformationBrick title="elevation" information={props.volcano.getElevation() + "m"} />
-                <InformationBrick title="last eruption" information={props.volcano.getLastEruption()} />
-            </div>
-        </div>
-    )
+  const pop100 = [1000];
+  const pop30 = [400];
+  const pop10 = [200];
+  const pop5 = [100];
+  const label = ['Population Spread'];
 
 
+
+  return (
+    <div className="row py-4">
+      <div className="col">
+        <InformationBrick 
+          title="country" 
+          information={props.volcano.Country} />
+        <InformationBrick 
+          title="region" 
+          information={props.volcano.Region} />
+        <InformationBrick
+          title="subregion"
+          information={props.volcano.Subregion}
+        />
+      </div>
+      <div className="col">
+        <InformationBrick
+          title="summit"
+          information={props.volcano.getSummit() + "m"}
+        />
+        <InformationBrick
+          title="elevation"
+          information={props.volcano.getElevation() + "m"}
+        />
+        <InformationBrick
+          title="last eruption"
+          information={props.volcano.getLastEruption()}
+        />
+      </div>
+      <BarChart
+        className="col"
+        width={500}
+        height={220}
+        series={[
+          { data: pop100, label: "100km", id: "pop100Id", stack: "total" },
+          { data: pop30, label: "30km", id: "pop30Id", stack: "total" },
+          { data: pop10, label: "10km", id: "pop10Id", stack: "total" },
+          { data: pop5, label: "5km", id: "pop5Id", stack: "total" },
+        ]}
+        xAxis={[{ data: label, scaleType: "band" }]}
+      />
+    </div>
+  );
 }
