d1ad99de3de52ec432f20cb63da891117d3bd92f
Logan <logan@codify.com>
2024-04-28 12:01:58 +1000

    Auto zooming the map and changing center when those values changes

--- cab230client/src/components/rightContainer/singleVolcano/Map.tsx
+++ cab230client/src/components/rightContainer/singleVolcano/Map.tsx
@@ -1,9 +1,21 @@
-import { Circle, MapContainer, TileLayer, Tooltip, Marker } from "react-leaflet";
+import {
+  Circle,
+  MapContainer,
+  TileLayer,
+  Tooltip,
+  Marker,
+  useMap,
+} from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import Volcano from "../../../packages/Volcano";
-import { LatLngTuple } from "leaflet";
+import { LatLng, LatLngTuple } from "leaflet";
 import { useContext, useEffect, useState } from "react";
-import { VolcanoContext, VolcanoContextType } from "../../../packages/Context";
+import {
+  VolcanoClientContext,
+  VolcanoClientContextType,
+  VolcanoContext,
+  VolcanoContextType,
+} from "../../../packages/Context";
 
 interface overlayProps {
   center: LatLngTuple;
@@ -14,13 +26,25 @@ function RadiusOverlays(props: overlayProps) {
   const fillBlueOptions = { fillColor: "blue" };
   return (
     <>
-      <Circle center={props.center} pathOptions={fillBlueOptions} radius={100000}>
+      <Circle
+        center={props.center}
+        pathOptions={fillBlueOptions}
+        radius={100000}
+      >
         <Tooltip>{props.population[3]} people</Tooltip>
       </Circle>
-      <Circle center={props.center} pathOptions={fillBlueOptions} radius={30000}>
+      <Circle
+        center={props.center}
+        pathOptions={fillBlueOptions}
+        radius={30000}
+      >
         <Tooltip>{props.population[2]} people</Tooltip>
       </Circle>
-      <Circle center={props.center} pathOptions={fillBlueOptions} radius={10000}>
+      <Circle
+        center={props.center}
+        pathOptions={fillBlueOptions}
+        radius={10000}
+      >
         <Tooltip>{props.population[1]} people</Tooltip>
       </Circle>
       <Circle center={props.center} pathOptions={fillBlueOptions} radius={5000}>
@@ -30,45 +54,67 @@ function RadiusOverlays(props: overlayProps) {
   );
 }
 
-
 export default function Map() {
-
   const { selectedVolcano } = useContext(VolcanoContext) as VolcanoContextType;
-  const [volcano] = useState<Volcano>(new Volcano(selectedVolcano));
-
-  const [zoomLevel, setZoomLevel] = useState<number>(12);
-  const center = volcano.getLatLngTuple();
-
-  console.log(volcano);
+  const { volcanoClient } = useContext(
+    VolcanoClientContext
+  ) as VolcanoClientContextType;
+  const [volcano, setVolcano] = useState<Volcano>(new Volcano(selectedVolcano));
+  const [center, setCenter] = useState<LatLngTuple>([0, 0]);
+  const [zoom, setZoom] = useState(12);
 
   useEffect(() => {
-    if (volcano.hasPopulationData()) {
-      setZoomLevel(8);
-      console.log("HELLO");
-    }
-    console.log("Outside");
-  }, [volcano]);
+    const getVolcanoFromApi = async () => {
+      const volcanoData: Volcano = await volcanoClient.getVolcanoById(
+        volcano.getId()
+      );
+      volcanoData.Id = volcano.getId();
+      setVolcano(volcanoData);
+      setCenter(volcanoData.getLatLngTuple());
+
+      if (volcanoData.hasPopulationData()) {
+        setZoom(8);
+      }
+    };
+
+    getVolcanoFromApi();
+  }, [volcanoClient]);
+
+  const RecenterAutomatically = ({center}: {center: LatLngTuple}) => {
+    const map = useMap();
+    useEffect(() => {
+      map.setView(center);
+    }, center);
+    return null;
+  };
+
+  const ZoomAutomatically = ({zoomLevel}: {zoomLevel: number}) => {
+    const map = useMap();
+    useEffect(() => {
+      map.setZoom(zoomLevel);
+    }, [zoomLevel]);
+    return null;
+  };
 
   function getRadiusInformation() {
-
     if (volcano.hasPopulationData()) {
-      return (
-        RadiusOverlays({
-          center: center,
-          population: volcano.getPopulationData()
-        })
-      );
+      return RadiusOverlays({
+        center: volcano.getLatLngTuple(),
+        population: volcano.getPopulationData(),
+      });
     }
     return null;
   }
-  
+
+  console.log("I have been rendered");
+  console.log(center);
 
   return (
     <div>
       <MapContainer
         className="leaflet-container"
         center={center}
-        zoom={zoomLevel}
+        zoom={zoom}
         scrollWheelZoom={true}
         style={{ width: "100%", height: "500px" }}
       >
@@ -78,6 +124,8 @@ export default function Map() {
         />
         {getRadiusInformation()}
         <Marker position={center}></Marker>
+        <RecenterAutomatically center={center} />
+        <ZoomAutomatically zoomLevel={zoom} />
       </MapContainer>
     </div>
   );
