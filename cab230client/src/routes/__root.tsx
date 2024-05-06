96f6d13b174099d7792ec413a4bdd873bace2b8d
Logan <logan@codify.com>
2024-05-07 08:30:51 +1000

    Fixed the null reference bug

--- cab230client/src/routes/__root.tsx
+++ cab230client/src/routes/__root.tsx
@@ -1,20 +1,6 @@
 import { createRootRoute, Outlet } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/router-devtools'
-import { createRouter } from '@tanstack/react-router'
 
-
-// Import the generated route tree
-import { routeTree } from '../routeTree.gen'
-
-// Create a new router instance
-const router = createRouter({ routeTree })
-
-// Register the router instance for type safety
-declare module '@tanstack/react-router' {
-  interface Register {
-    router: typeof router
-  }
-}
 
 export const Route = createRootRoute({
     component: RootComponent,
@@ -23,7 +9,6 @@ export const Route = createRootRoute({
 function RootComponent() {
     return (
         <div>
-        hello
         <Outlet />
         <TanStackRouterDevtools />
         </div>
