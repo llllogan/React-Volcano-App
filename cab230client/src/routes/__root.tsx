1e283344f48d9d82ccb3626c88d3477680473f77
Logan <logan@codify.com>
2024-05-07 19:48:04 +1000

    Added the contents of app to the __root file

--- cab230client/src/routes/__root.tsx
+++ cab230client/src/routes/__root.tsx
@@ -1,5 +1,17 @@
 import { createRootRoute, Outlet } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/router-devtools'
+import "../App.css";
+import { User, Country, IVolcano } from "../packages/Interfaces";
+import {
+  UserContext,
+  CountryContext,
+  VolcanoSelectedContext,
+  VolcanoContext,
+  VolcanoClientContext,
+} from "../packages/Context";
+import { useState } from "react";
+import VolcanoApiClient from "../packages/VolcanoClient";
+import LeftPanelContainer from "../components/leftContainer/LeftContainer";
 
 
 export const Route = createRootRoute({
@@ -7,11 +19,52 @@ export const Route = createRootRoute({
 })
 
 function RootComponent() {
+
+    const loggedOutUser: User = {
+        name: "",
+        email: "",
+        isLoggedIn: false,
+      };
+    
+      const [currentUser, setCurrentUser] = useState(loggedOutUser);
+      const [selectedCountry, setSelectedCountry] = useState({} as Country);
+      const [selectedVolcano, setSelectedVolcano] = useState({} as IVolcano);
+      const [volcanoSelected, setVolcanoSelected] = useState(false);
+      const [volcanoClient, setVolcanoClient] = useState(new VolcanoApiClient({}));
+
+
     return (
-        <div>
-        <Outlet />
-        <TanStackRouterDevtools />
+        <div className="container-fluid App" id="app-container">
+          <VolcanoClientContext.Provider
+            value={{ volcanoClient, setVolcanoClient }}
+          >
+            <UserContext.Provider 
+              value={{ currentUser, setCurrentUser }}
+            >
+              <CountryContext.Provider
+                value={{ selectedCountry, setSelectedCountry }}
+              >
+                <VolcanoSelectedContext.Provider
+                  value={{ volcanoSelected, setVolcanoSelected }}
+                >
+                  <VolcanoContext.Provider
+                    value={{ selectedVolcano, setSelectedVolcano }}
+                  >
+                    <div className="row">
+                      <div className="col" id="leftPanel">
+                        <LeftPanelContainer />
+                      </div>
+                      <div className="col-9">
+                      <Outlet />
+                      </div>
+                    </div>
+                    <TanStackRouterDevtools />
+                  </VolcanoContext.Provider>
+                </VolcanoSelectedContext.Provider>
+              </CountryContext.Provider>
+            </UserContext.Provider>
+          </VolcanoClientContext.Provider>
         </div>
-    )
+      );
 }
 
