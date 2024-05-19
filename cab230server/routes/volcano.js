c78179a49b39cb5c27b1db6064bd15093a052d63
Logan <logan@codify.com>
2024-05-19 15:48:31 +1000

    Added new routes for eruptions and reviews in volcano.js

--- cab230server/routes/volcano.js
+++ cab230server/routes/volcano.js
@@ -11,4 +11,21 @@ router.get("/:id", (req, res) => {
     );
 });
 
+router.route("/:id/eruptions")
+    .get((req, res) => {})
+    .post((req, res) => {});
+
+
+router.route("/:id/reviews")
+    .get((req, res) => {})
+    .post((req, res) => {});
+
+router.param("id", (req, res, next, id) => {
+    console.log("Trying to find volcano with id of: " + id)
+    req.id = id;
+    next();
+});
+
+
+
 module.exports = router;
