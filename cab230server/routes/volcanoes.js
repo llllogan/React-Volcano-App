52e379a1703a4ffbeab86a963270499d63072e3f
Logan <logan@codify.com>
2024-05-20 14:46:22 +1000

    Added a knex connection

--- cab230server/routes/volcanoes.js
+++ cab230server/routes/volcanoes.js
@@ -1,6 +1,18 @@
 const express = require("express");
 const router = express.Router();
 
+const knex = require('knex')({
+    client: 'mysql',
+    version: '8.3.0',
+    connection: {
+      host: '127.0.0.1',
+      port: 3306,
+      user: 'root',
+      password: 'admin',
+      database: 'volcanoes',
+    },
+  });
+
 router.get("/" , (req, res) => {});
 
 module.exports = router;
