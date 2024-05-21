2cab8f2e0cf0f52caf5e4afc0474e2893dba026e
Logan <logan@codify.com>
2024-05-21 19:31:40 +1000

    Returning volcanoes of a country

--- cab230server/routes/volcanoes.js
+++ cab230server/routes/volcanoes.js
@@ -1,6 +1,18 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/" , (req, res) => {});
+router.get("/" , async (req, res) => {
+
+    const country = req.getQueryParamWithName("country");
+
+    if (country == null) {
+        res.sendError("Country is a required query parameter.");
+    }
+    
+    const data = await req.db.getVolcanoesInCountry(country);
+    console.log(data);
+
+    res.sendSuccess(data);
+});
 
 module.exports = router;
